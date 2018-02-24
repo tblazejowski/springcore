8012009ba6574497714dca05cca54a6b894bf316
tblazejowski <tomasz.blazejowski@gmail.com>
2018-02-24 22:21:47 +0100

    Removed usage of the MessageBuilder from tests

--- src/test/java/com/blazej/messagesender/domain/MessageTest.java
+++ src/test/java/com/blazej/messagesender/domain/MessageTest.java
@@ -1,5 +1,6 @@
 package com.blazej.messagesender.domain;
 
+import com.blazej.messagesender.service.MessageDto;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,15 +9,17 @@ public class MessageTest {
     @Test
     public void shouldCreateNewMessageWithRequiredArgs () {
 
-        Message message = new Message.MessageBuilder()
-                .withSubject("hot topic")
-                .withBody("nice body")
-                .withSender("from someone")
-                .withRecipient("to someone").build();
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("hot topic");
+        messageDto.setBody("nice body");
+        messageDto.setSender("from someone");
+        messageDto.setRecipient("from someone");
 
-        Assert.assertEquals(message.getSubject(), "hot topic");
-        Assert.assertEquals(message.getBody(), "nice body");
-        Assert.assertEquals(message.getSender(), "from someone");
-        Assert.assertEquals(message.getRecipient(), "to someone");
+        Message message = new MessageFactory().createFrom(messageDto);
+
+        Assert.assertEquals(messageDto.getSubject(), message.getSubject());
+        Assert.assertEquals(messageDto.getBody(), message.getBody());
+        Assert.assertEquals(messageDto.getSender(), message.getSender());
+        Assert.assertEquals(messageDto.getRecipient(), message.getRecipient());
     }
 }
