d2b3fc3252223f788a23fa5c029a99b9e279ae07
tblazejowski <tomasz.blazejowski@gmail.com>
2018-02-27 23:41:15 +0100

    Useless constructor and setters removed from Message class

--- src/main/java/com/blazej/messagesender/domain/Message.java
+++ src/main/java/com/blazej/messagesender/domain/Message.java
@@ -9,19 +9,6 @@ public class Message {
     private String recipient = NO_VALUE;
     private String id = NO_VALUE;
 
-    public Message(String subject, String body) {
-        this.subject = subject;
-        this.body = body;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public void setRecipient(String recipient) {
-        this.recipient = recipient;
-    }
-
     public void setId(String id) {
         this.id = id;
     }
